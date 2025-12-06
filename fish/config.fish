43f2bd976ec123d4384f625598155acc44fa6101
TGYT-Dev <zeke0949@gmail.com>
2025-12-06 12:51:13 -0500

    Made sober alias a function, you can now update from the alias using -u

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,29 @@
 if status is-interactive
-    
-  # Astetichs
 
-  zfetch
-  
-  # Alias - Maybe make them actual scripts later
-  
-  alias roblox 'flatpak run org.vinegarhq.Sober'
-  alias sober 'flatpak run org.vinegarhq.Sober'
-  alias cat 'bat --theme="gruvbox-dark" --style=numbers,grid' 
-  alias ls 'eza --icons --no-permissions --no-user --no-time --no-filesize'
-  alias chrome google-chrome-stable
-  alias google google-chrome-stable
-  alias vencordInject 'sh -c "$(curl -sS https://vencord.dev/install.sh)"'
-  alias nano nvim
-  # Commands to run in interactive sessions can go here
+    # Astetichs
+
+    zfetch
+
+    # Alias - Maybe make them actual scripts later
+
+    alias roblox sober
+    alias cat 'bat --theme="gruvbox-dark" --style=numbers,grid'
+    alias ls 'eza --icons --no-permissions --no-user --no-time --no-filesize'
+    alias chrome google-chrome-stable
+    alias google google-chrome-stable
+    alias vencordInject 'sh -c "$(curl -sS https://vencord.dev/install.sh)"'
+    alias nano nvim
+
+    # Functions
+
+    function sober
+        if test "$argv[1]" = -u
+            flatpak update org.vinegarhq.Sober
+        else
+            flatpak run org.vinegarhq.Sober
+        end
+
+    end
+
 end
 starship init fish | source
